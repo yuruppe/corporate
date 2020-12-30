f2cc4eea3cdadafa76f7692ddb00b28809fee22e
ryota.ito <itomise.mizumi@icloud.com>
2020-12-30 13:11:58 +0900

    update : gtm moduleに変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,13 @@ import { Global } from '@emotion/react'
 import { globalStyles } from '~/styles/global'
 import { Header } from '~/components/layout/Header'
 import { Container } from '~/components/layout/Container'
+import TagManager from 'react-gtm-module'
+import { useEffectOnce } from '~/hooks/useEffectOnce'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  useEffectOnce(() => {
+    TagManager.initialize({ gtmId: 'GTM-MKB3PLP' })
+  })
   return (
     <>
       <Global styles={globalStyles} />
