5b30775a8e504681adf190ab1fc496a0be156b0e
ryota.ito <itomise.mizumi@icloud.com>
2020-12-30 13:06:21 +0900

    update : gtm 追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,8 +19,25 @@ class CustomDocument extends Document {
             rel="stylesheet"
           />
           <script src="/vendor/picturefill.min.js" async />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MKB3PLP');`,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MKB3PLP"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
